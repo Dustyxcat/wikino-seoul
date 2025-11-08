0dd7cdfd2217c243aafd7670dc866eba7a761e5e
Dustyxcat <smr623@naver.com>
2025-11-09 02:11:05 +0900

    97% end
    스와이퍼, aos, 미디어쿼리 1차 확인 완료

--- js/main.js
+++ js/main.js
@@ -8,5 +8,48 @@ $(document).ready(function(){
             clickable: true,
         },
     });
+    
+    var swiper = new Swiper(".mob_1_swiper", {
+        loop: true,
+        autoplay: true,
+    });
+
+    var swiper = new Swiper(".mob_swiper_2", {
+        direction: "vertical",
+        loop: true,
+        autoplay: true,
+    });
+
+    var swiper = new Swiper(".mob_swiper_5", {
+        direction: "vertical",
+        loop: true,
+        autoplay: true,
+    });
+
+    $('.ham_wrap').click(function(){
+
+        $('.del_dot').toggleClass('del_dot-active')
+        $('.dot_1').toggleClass('dot_1-active')
+        $('.dot_2').toggleClass('dot_2-active')
+        $('.split_page').toggleClass('split-active')
+        
+    });
+
+    $(window).scroll(function(){
+        var pageY = $(window).scrollTop()
+         if(pageY > 300){
+
+            $(".mob_header").css({
+                position: 'fixed',
+                zIndex: 60,
+                backgroundColor: '#ffffffff',
+            })
+         }else if(pageY < 300){
+            $(".mob_header").css({
+                position: 'absolute',
+                backgroundColor: '#FFFFFF00'
+            })
+         }
+    });
 
 }); //end
